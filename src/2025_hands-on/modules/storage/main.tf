6bdf6100beed3dae679c0f0013221e0d0d38168c
cocoa-maemae <breezin_ahimusa@aol.com>
2025-06-04 17:36:21 +0900

    fix continuous validation

--- src/2025_hands-on/modules/storage/main.tf
+++ src/2025_hands-on/modules/storage/main.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm = "aws:kms"
     }
   }
 }
